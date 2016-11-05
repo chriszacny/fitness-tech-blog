a6a079c57bb46e713dbb3583f4a4009fee523aa2
Chris Zacny <czacny@gmail.com>
2016-11-05 15:20:30 -0700

    save point

--- site/passenger_wsgi.py
+++ site/passenger_wsgi.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from flask_flatpages import FlatPages
+
+
+FLATPAGES_AUTO_RELOAD = True
+FLATPAGES_EXTENSION = '.md'
+FLATPAGES_ROOT = 'content'
+BLOG_DIR = 'blog'
 
 
 application = Flask(__name__)
+flatpages = FlatPages(application)
 
 
 @application.route('/')
@@ -9,12 +17,31 @@ application = Flask(__name__)
 @application.route('/home/')
 @application.route('/blog/')
 def blog():
-    # TODO: Temporary, eventually call index or blog.html when template overrides are done
-    return render_template('base_template.html')
+    return render_template('blog_posts.html', posts=get_all_posts())
+
+
+@application.route('/blog/<slugname>/')
+def post(slugname):
+    post = get_post_url_by_name(slugname)
+    posts = [post]
+    return render_template('blog_posts.html', posts=posts)
+
+
+def get_post_url_by_name(name):
+    path = '{}/{}'.format(BLOG_DIR, name)
+    post = flatpages.get_or_404(path)
+    return post
+
+
+def get_all_posts():
+    posts = [p for p in flatpages if p.path.startswith(BLOG_DIR)]
+    posts.sort(key=lambda item:item['date'], reverse=True)
+    return posts
 
 
 def main():
     application.config.from_pyfile('settings.py', silent=True)
+    flatpages.init_app(application)
     application.run(host=application.config['HOST'], debug=True)
 
 
