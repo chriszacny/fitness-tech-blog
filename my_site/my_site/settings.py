e0dab2586fdc58650fb062039a489588de08af25
Chris Zacny <czacny@gmail.com>
2016-11-25 17:52:41 -0500

    save point

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -2,7 +2,8 @@
 HOST = '127.0.0.1'
 
 # Site Constants
-SITE_TITLE = 'Zacny Fitness Blog'
+SITE_TITLE = 'A Run in the Forest'
+SITE_DESCRIPTION = 'Seeking Good Health in the Forests and Mountains'
 HOME_PAGE_ARTICLES_PER_PAGE_COUNT = 3
 
 # Flat Pages Configuration
